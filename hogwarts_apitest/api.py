ecf9d5f61696b6deaad46bcd610068e5b8ba5aca
huqiang252 <huqiang252@gmail.com>
2021-01-23 16:19:37 +0800

    实现单个Http(s)接口的测试003

--- hogwarts_apitest/api.py
+++ hogwarts_apitest/api.py
@@ -31,16 +31,20 @@ class BaseApi(object):
                                          data=self.data,
                                          json=self.json
                                          )
-        return self  # 为了链式调用
+        return self
 
     def validate(self,key,expected_value):
         value = self.response
         for _key in key.split("."):
-            # print("调试==>", _key, value,type(value), expected_value)
+            # print("调试前==>", _key, value,type(value), expected_value)
             if isinstance(value,requests.Response):
-                value = getattr(value, _key)
-            elif isinstance(value,requests.structures.CaseInsensitiveDict):
+                if _key=="json()":
+                    value=self.response.json()
+                else:
+                    value = getattr(value, _key)
+            elif isinstance(value,(requests.structures.CaseInsensitiveDict,dict)):
                 value=value[_key]
+
 
         assert value==expected_value
         return self
--- tests/test_core.py
+++ tests/test_core.py
@@ -29,19 +29,27 @@ def test_httpbin_get():
     ApiHttpbinGit().run()\
         .validate("status_code",200)\
         .validate("headers.server","gunicorn/19.9.0")\
-        # .validate("json.url","http://httpbin.org/get")
+        .validate("json().url","http://httpbin.org/get")\
+        .validate("json().args",{})\
+        .validate("json().headers.Accept","application/json")
+
 
 
 def test_httpbin_get_with_prams():
 
     ApiHttpbinGit().set_params(abc=123,xyz=456).run()\
         .validate("status_code",200) \
-        .validate("headers.server", "gunicorn/19.9.0")
+        .validate("headers.server", "gunicorn/19.9.0")\
+        .validate("json().url","http://httpbin.org/get?abc=123&xyz=456") \
+        .validate("json().headers.Accept", "application/json")
+
 
 def test_httpbin_post():
     ApiHttpBinPost()\
-        .set_data({"yyy":999})\
+        .set_json({"yyy":999})\
         .run()\
         .validate("status_code",200) \
-        .validate("headers.server", "gunicorn/19.9.0")
+        .validate("headers.server", "gunicorn/19.9.0") \
+        .validate("json().headers.Accept", "application/json")\
+        .validate("json().json.yyy",999)
 
